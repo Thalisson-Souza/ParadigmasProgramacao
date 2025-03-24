559884c89a6484b32548d659212042eb36f1be68
Thalisson Souza <thalisson.souza@acad.ufsm.br>
2025-03-24 19:24:18 -0300

    fix: delta formula

--- src/Trabalho_1/Exercicio04_FormulaBhaskara.java
+++ src/Trabalho_1/Exercicio04_FormulaBhaskara.java
@@ -1,4 +1,4 @@
-package exercicios.Trabalho_1;
+package Trabalho_1;
 
 import java.util.Scanner;
 
@@ -6,15 +6,15 @@ public class Exercicio04_FormulaBhaskara {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Digite os 3 coeficientes de uma equação de segundo grau: A, B e C respectivamente: ");
+        System.out.println("Digite os 3 coeficientes de uma equação de segundo grau (A, B e C), respectivamente: ");
         var coeficienteA = scanner.nextInt();
         var coeficienteB = scanner.nextInt();
         var coeficienteC = scanner.nextInt();
 
         var delta = (Math.pow(coeficienteB, 2) ) - 4 * coeficienteA * coeficienteC;
-
-        var xLinha = ( - coeficienteB + delta) / (2 * coeficienteA);
-        var xDuasLinha = ( - coeficienteB - delta) / (2 * coeficienteA);
+        var raizDelta = Math.sqrt(delta);
+        var xLinha = ( -coeficienteB + raizDelta) / (2 * coeficienteA);
+        var xDuasLinha = ( -coeficienteB - raizDelta) / (2 * coeficienteA);
 
         if(delta < 0) {
             System.out.println("A equação: " + coeficienteA + "x^2 + " + coeficienteB + "x + " + coeficienteC + " não tem raizes reais!!");
